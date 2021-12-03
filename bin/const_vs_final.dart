a17ab748d1223d4f0ce33618997fda2ef892e4cb
DILSHA-SAJAN <345567@manappuram.com>
2021-12-03 10:39:23 +0530

    first commit

---
+++ bin/const_vs_final.dart
@@ -0,0 +1,13 @@
+void main(List<String> arguments) {
+  // final String name;
+  // name = 'Some name';
+
+  // final List<int> numberList = [];
+
+  // numberList.add(1);
+  // numberList.add(100);
+
+  // print(numberList);
+
+  const String name = 'Dilsha'; // declaration should be done whew u initialise
+}
